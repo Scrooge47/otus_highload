403fcf249409b57b0f8b20de4e1a03906e7c8729
Alex Avseenkov <a.avseenkov@yandex.ru>
2024-05-21 11:14:27 +0300

    Homework 7. In-memory DDMS

--- social/src/main/java/ru/avseenkov/social/SocialApplication.java
+++ social/src/main/java/ru/avseenkov/social/SocialApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SocialApplication  {
 
-    public static void main(String[] args) t{
+    public static void main(String[] args) {
         SpringApplication.run(SocialApplication.class, args);
     }
 
